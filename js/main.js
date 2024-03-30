875396612bbe2fdab798a68dea330842a2e1fdde
yelose <yelose85@gmail.com>
2024-03-30 18:27:26 +0100

    added presentation.html content and styles, followed SOLID principle refactoring main.js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,4 @@
-function showSection(section, element) {
-    clearSections()
-    document.querySelectorAll('.active').forEach(item =>{
-        item.classList.remove('active')
-    })
-    element.classList.add("active")
-    
+function getSectionHtml(section) {
     const sectionHtml = document.getElementById(section)
     sectionHtml.classList.add("show-section")
     fetch(`./pages/${section}.html`).then(res => res.text()).then(html => sectionHtml.innerHTML = html)
@@ -12,8 +6,26 @@ function showSection(section, element) {
 
 function clearSections() {
     const sections = document.querySelectorAll('.show-section')
-    sections.forEach(section => {
-        section.classList.remove('show-section')
-    });
+    if (sections) {
+        sections.forEach(section => {
+            section.classList.remove('show-section')
+        });
+    }
 }
+
+function activeCurrentMenuOption(element){
+    document.querySelectorAll('.active').forEach(item => {
+        item.classList.remove('active')
+    })
+    element.classList.add("active")
+}
+
+function showSection(section, element) {
+    clearSections()
+    if (element) {
+        activeCurrentMenuOption(element)
+    }
+    getSectionHtml(section)
+}
+
 showSection('presentation')
